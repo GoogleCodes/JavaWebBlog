415496e6b21bc88e25e36ecc2d779a5c920fe49c
‘511810970@qq.com <511810970@qq.com>
2018-02-08 16:17:08 +0800

    update

--- Blog/WebRoot/go/pages/upload/upload.js
+++ Blog/WebRoot/go/pages/upload/upload.js
@@ -12,7 +12,7 @@ Page({
   data: {
     imgArrays: [],
     imagesVisible: true,
-    textarea: '测试'
+    textarea: 'some thing...'
   },
 
   /**
@@ -72,7 +72,7 @@ Page({
     console.log($.formatTime(new Date()), that.data.textarea);
     for (let i in that.data.imgArrays) {
       $.uploadAjax({
-        url: 'FileUploadServlet?desc=' + "that.data.textarea" + '&openid=' + wx.getStorageSync('wxUserInfo').openid,
+        url: 'FileUploadServlet?desc=' + that.data.textarea + '&openid=' + wx.getStorageSync('wxUserInfo').openid,
         filePath: that.data.imgArrays[i],
         formData: {
           
@@ -107,49 +107,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage() {
 
   }
 })
